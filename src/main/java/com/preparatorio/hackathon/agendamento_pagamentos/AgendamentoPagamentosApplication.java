c421b7a269ddc1f763d3ff72dea5aa5146e5d9ae
Bruno Henrique Ramos <bruno.hr.gay@gmail.com>
2025-04-23 10:04:42 -0300

    :tada: criação do projeto

---
+++ src/main/java/com/preparatorio/hackathon/agendamento_pagamentos/AgendamentoPagamentosApplication.java
@@ -0,0 +1,13 @@
+package com.preparatorio.hackathon.agendamento_pagamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoPagamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoPagamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/preparatorio/hackathon/agendamento_pagamentos/AgendamentoPagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.preparatorio.hackathon.agendamento_pagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoPagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
