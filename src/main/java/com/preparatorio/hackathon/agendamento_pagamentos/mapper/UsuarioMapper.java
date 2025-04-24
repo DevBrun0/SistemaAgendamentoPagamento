729f7acd0e44934ca530ab9d48c79d44ee1ea53e
tulio <tulio.jacinto32@gmail.com>
2025-04-24 10:32:46 -0300

    feat: UsuarioMapper

---
+++ src/main/java/com/preparatorio/hackathon/agendamento_pagamentos/mapper/UsuarioMapper.java
@@ -0,0 +1,25 @@
+package com.preparatorio.hackathon.agendamento_pagamentos.mapper;
+
+import com.preparatorio.hackathon.agendamento_pagamentos.api.usuario.request.CriarUsuarioRequest;
+import com.preparatorio.hackathon.agendamento_pagamentos.api.usuario.response.UsuarioResponse;
+import com.preparatorio.hackathon.agendamento_pagamentos.domain.Usuario;
+
+public class UsuarioMapper {
+    public Usuario criarUsuarioRequestToUsuario(CriarUsuarioRequest criarUsuarioRequest) {
+        return new Usuario(
+                criarUsuarioRequest.nome(),
+                criarUsuarioRequest.documento(),
+                criarUsuarioRequest.email(),
+                criarUsuarioRequest.senha()
+        );
+    }
+
+    public UsuarioResponse usuarioToUsuarioResponse(Usuario usuario) {
+        return new UsuarioResponse(
+                usuario.getId(),
+                usuario.getNome(),
+                usuario.getDocumento(),
+                usuario.getEmail()
+        );
+    }
+}
